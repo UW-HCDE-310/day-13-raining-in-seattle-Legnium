13c578137c9753aa9a2100628b3de243fe648562
Legnium <angelt22@uw.edu>
2024-11-12 14:14:57 -0800

    Is it raining in Seattle?

---
+++ app.py
@@ -0,0 +1,17 @@
+import urllib.parse, urllib.request, urllib.error, json, flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def is_it_raining_in_seattle():
+    with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
+        is_it_raining_in_seattle = response.read().decode()
+
+    if is_it_raining_in_seattle == "true":
+        return '<h1>Yes</h1>'
+    else:
+        return '<h1>No</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
